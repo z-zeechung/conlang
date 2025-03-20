7645e326352103a372a0974d9944c13823d9f0e3
z-zeechung <zhouzeechung@qq.com>
2025-03-20 09:17:19 +0800

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { Avatar, Button, Card, Flex, Tag, Typography } from 'antd'
+import { Avatar, Button, Card, Flex, List, Tag, Typography } from 'antd'
 import Tools from "./tools.json"
-import { AndroidFilled, AppleFilled, BaiduOutlined, GithubFilled, GlobalOutlined, LinuxOutlined, MailOutlined, QqOutlined, WindowsFilled} from '@ant-design/icons'
+import { AndroidFilled, AppleFilled, BaiduOutlined, GithubFilled, GlobalOutlined, LinuxOutlined, MailOutlined, WindowsFilled} from '@ant-design/icons'
+import { useWindowSize } from './utils'
 
 function App() {
+
+  const isMobile = useWindowSize().width <= 600
+
   return <Flex vertical align='center' gap={"middle"}>
     <Flex vertical align='center' >
       <Typography.Title level={2} style={{fontFamily:"serif"}}>造语工具</Typography.Title>
@@ -27,43 +31,83 @@ function App() {
         </a>
       </Typography.Text>
     </Flex>
-    <Card title="单词生成" style={{width:"100%"}}>
-      <Flex gap={"small"} wrap>
-        {Tools.word_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
-      </Flex>
-    </Card>
-    <Card title="字体制作" style={{width:"100%"}}>
-      <Flex gap={"small"} wrap>
-        {Tools.font_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
-      </Flex>
-    </Card>
-    <Card title="输入法制作" style={{width:"100%"}}>
-      <Flex gap={"small"} wrap>
-        {Tools.ime_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
-      </Flex>
-    </Card>
-    <Card title="词典编纂" style={{width:"100%"}}>
-      <Flex gap={"small"} wrap>
-        {Tools.dict_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
-      </Flex>
-    </Card>
-    <Card title="码表" style={{width:"100%"}}>
-      <Flex gap={"small"} wrap>
-        {Tools.code_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
-      </Flex>
-    </Card>
-    <Card title="翻译器" style={{width:"100%"}}>
-      <Flex gap={"small"} wrap>
-        {Tools.translate_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
-      </Flex>
-    </Card>
+    {!isMobile&&<>
+      <Card title="单词生成" style={{width:"100%"}}>
+        <Flex gap={"small"} wrap>
+          {Tools.word_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
+        </Flex>
+      </Card>
+      <Card title="字体制作" style={{width:"100%"}}>
+        <Flex gap={"small"} wrap>
+          {Tools.font_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
+        </Flex>
+      </Card>
+      <Card title="输入法制作" style={{width:"100%"}}>
+        <Flex gap={"small"} wrap>
+          {Tools.ime_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
+        </Flex>
+      </Card>
+      <Card title="词典编纂" style={{width:"100%"}}>
+        <Flex gap={"small"} wrap>
+          {Tools.dict_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
+        </Flex>
+      </Card>
+      <Card title="码表" style={{width:"100%"}}>
+        <Flex gap={"small"} wrap>
+          {Tools.code_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
+        </Flex>
+      </Card>
+      <Card title="翻译器" style={{width:"100%"}}>
+        <Flex gap={"small"} wrap>
+          {Tools.translate_tools.map((tool, idx)=><ToolCard tool={tool} key={idx}/>)}
+        </Flex>
+      </Card>
+    </>}
+    {isMobile&&<>
+      <List
+        header={<Typography.Title level={4}>单词生成</Typography.Title>}
+        style={{width:"95%"}}
+        dataSource={Tools.word_tools}
+        renderItem={renderItem}
+      />
+      <List
+        header={<Typography.Title level={4}>字体制作</Typography.Title>}
+        style={{width:"95%"}}
+        dataSource={Tools.font_tools}
+        renderItem={renderItem}
+      />
+      <List
+        header={<Typography.Title level={4}>输入法制作</Typography.Title>}
+        style={{width:"95%"}}
+        dataSource={Tools.ime_tools}
+        renderItem={renderItem}
+      />
+      <List
+        header={<Typography.Title level={4}>词典编纂</Typography.Title>}
+        style={{width:"95%"}}
+        dataSource={Tools.dict_tools}
+        renderItem={renderItem}
+      />
+      <List
+        header={<Typography.Title level={4}>码表</Typography.Title>}
+        style={{width:"95%"}}
+        dataSource={Tools.code_tools}
+        renderItem={renderItem}
+      />
+      <List
+        header={<Typography.Title level={4}>翻译器</Typography.Title>}
+        style={{width:"95%"}}
+        dataSource={Tools.translate_tools}
+        renderItem={renderItem}
+      />
+    </>}
   </Flex>
 }
 
 function ToolCard(props: { tool: any}) {
   const tool = props.tool
   return <Card 
-    style={{width:400}}
+    style={{width:380}}
     title={<Flex gap={"middle"} align='center'>
       {tool?.icon?<Avatar src={tool.icon} shape={"square"}/>:undefined}
       {tool?.name}
@@ -84,6 +128,28 @@ function ToolCard(props: { tool: any}) {
       </Flex>
     </Flex>
   </Card>
+}
+function renderItem(item:any, idx:number){
+  return <List.Item key={idx}>
+    <List.Item.Meta
+      title={item.name}
+      description={<>
+        {item?.author&&<a href={item?.authorpage?item.authorpage:undefined} target="_blank"><Tag>{item.author}</Tag></a>}
+        {item.homepage&&<a href={item.homepage} target="_blank"><Tag color='green' icon={<GlobalOutlined/>}>主页</Tag></a>}
+      </>}
+      avatar={item?.icon?<Avatar src={item.icon} shape={"square"}/>:undefined}
+    />
+    <p/>
+    {item?.introduction}
+    <p/>
+    <Flex gap={"small"} wrap>
+      {item?.downloads?.windows&&<ToolDownloadButton url={item.downloads.windows} platform="windows"/>}
+      {item?.downloads?.linux&&<ToolDownloadButton url={item.downloads.linux} platform="linux"/>}
+      {item?.downloads?.macos&&<ToolDownloadButton url={item.downloads.macos} platform="macos"/>}
+      {item?.downloads?.android&&<ToolDownloadButton url={item.downloads.android} platform="android"/>}
+      {item?.downloads?.ios&&<ToolDownloadButton url={item.downloads.ios} platform="ios"/>}
+    </Flex>
+  </List.Item>
 }
 function ToolDownloadButton(props:{url:any, platform:string}){
   const icon = {
